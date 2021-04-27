1dd19d7611a6af2eda4af4a1af95cc5a1d44c97f
keysmusician <justin.masayda@gmail.com>
2021-04-27 23:35:24 +0000

    Remove chr()

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,8 @@
 def uppercase(str):
     for c in str:
         if ord(c) in range(ord('a'), ord('z') + 1):
-            c = chr(ord(c) - 32)
-        print(c, end='')
+            n = ord(c) - 32
+        else:
+            n = ord(c)
+        print("{:c}".format(n), end='')
     print('')
