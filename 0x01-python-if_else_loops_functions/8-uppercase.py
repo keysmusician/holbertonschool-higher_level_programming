cee45d37625c1dd07c50e00aa293ad2b51bfc3ad
keysmusician <justin.masayda@gmail.com>
2021-04-27 22:48:20 +0000

    Fix check for lowercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if c in range(ord('a'), ord('z') + 1):
+        if ord(c) in range(ord('a'), ord('z') + 1):
             print("{:c}".format(ord(c) - 32), end='')
     print()
