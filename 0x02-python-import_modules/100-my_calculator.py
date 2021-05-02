f484471cad24d6fdbb0257ba92bbc5278819ec0c
keysmusician <justin.masayda@gmail.com>
2021-05-02 03:06:31 +0000

    Check if main module issue

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,8 @@
-
+#!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 import sys
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
     if len(sys.argv[1:]) is not 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
