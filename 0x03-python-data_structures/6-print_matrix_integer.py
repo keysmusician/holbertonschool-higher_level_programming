1a478697c1e68c867e6249ac730800e9e7b8d5e7
keysmusician <justin.masayda@gmail.com>
2021-05-04 19:05:13 +0000

    Without enumerate()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i, n in enumerate(row):
+        for i in range(len(row)):
             if i != 0:
-                print(" {:d}".format(n), end="")
+                print(" {:d}".format(row[i]), end="")
             else:
-                print("{:d}".format(n), end="")
+                print("{:d}".format(row[i]), end="")
         print()
 
 matrix = [
