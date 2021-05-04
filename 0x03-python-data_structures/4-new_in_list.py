443d982d2a50ca236d6ff748bcb3b64636206361
keysmusician <justin.masayda@gmail.com>
2021-05-04 18:39:54 +0000

    Update index range check

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = list(my_list)
-    if 0 <= idx <= len(new_list):
+    if 0 <= idx < len(new_list):
         new_list[idx] = element
     return new_list
