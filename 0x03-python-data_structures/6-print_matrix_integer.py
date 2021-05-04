035ed1cce9580affed9c7a9eb217fffdf0f04b55
keysmusician <justin.masayda@gmail.com>
2021-05-04 19:13:04 +0000

    Switch back to enumerate()

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
+        for i, n in enumerate(row):
             if i != 0:
-                print(" {:d}".format(row[i]), end="")
+                print(" {:d}".format(n), end="")
             else:
-                print("{:d}".format(row[i]), end="")
+                print("{:d}".format(n), end="")
         print('')
