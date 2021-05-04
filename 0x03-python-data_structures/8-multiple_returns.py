69cb3614e2dc3be9bd3ceb4ffea4653f419358e3
keysmusician <justin.masayda@gmail.com>
2021-05-04 20:23:49 +0000

    Ensure empty sentance returns None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), sentence[:1])
+    if len(sentence):
+        return (len(sentence), sentence[:1])
+    else:
+        return (0, None)
