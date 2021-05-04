cc4c239a55452eae4078e9b9d2e31eae9ec1a391
keysmusician <justin.masayda@gmail.com>
2021-05-04 18:58:28 +0000

    Update whitespace format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, n in enumerate(row):
             if i != 0:
-                print(" ", end="")
-            print("{:d}".format(n), end="")
+                print(" {:d}".format(n), end="")
+            else:
+                print("{:d}".format(n), end="")
         print()
 
 matrix = [
