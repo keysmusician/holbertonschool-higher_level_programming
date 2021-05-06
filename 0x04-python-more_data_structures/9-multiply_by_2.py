6fade70011664d80ef420209d193d56922eea2c5
keysmusician <justin.masayda@gmail.com>
2021-05-06 19:11:02 +0000

    Format

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
- def multiply_by_2(a_dictionary):
-     """Return a new dictionary with all values multiplied by 2."""
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """Return a new dictionary with all values multiplied by 2."""
     return dict([(key, val * 2) for key, val in a_dictionary.items()])
