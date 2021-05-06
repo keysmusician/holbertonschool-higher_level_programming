9488c946dff41b2188767c131a1e2ceb39be995a
keysmusician <justin.masayda@gmail.com>
2021-05-06 20:20:07 +0000

    Retry task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Return a key with the biggest integer value."""
-    return a_dictionary[max(a_dictionary)] if a_dictionary else None
+    if a_dictionary:
+        return max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
