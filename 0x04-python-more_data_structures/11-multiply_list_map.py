66c5e08b2667922c70ca055a1fa320340c1d657d
keysmusician <justin.masayda@gmail.com>
2021-05-06 19:30:33 +0000

    Format docstring

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """Return a list of values multiplied by 'number.'
-
-    Return a new list of integers multiplied by 'number' without using any
-    loops.
-    """
+    """Return a list of values multiplied by 'number' without using loops."""
     return list(map(lambda x: x * number, my_list))
