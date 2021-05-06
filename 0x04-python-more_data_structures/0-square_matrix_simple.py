0935e6c6d1ae3b57223b226fe744954659c3a593
keysmusician <justin.masayda@gmail.com>
2021-05-06 17:08:01 +0000

    Format & edit docstring

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    """Return the square value of all integers of a matrix as a new matrix."""
-    return [list(map(lambda x : x**2, row)) for row in matrix]
+    """Return a new matrix with the square of all integers of a matrix."""
+    return [list(map(lambda x: x**2, row)) for row in matrix]
 
 matrix = [
     [1, 2, 3],
