0052a2ab77493546a1761f792e68619d98b0cef9
keysmusician <justin.masayda@gmail.com>
2021-05-06 17:09:58 +0000

    Remove tests

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,13 +2,4 @@
 def square_matrix_simple(matrix=[]):
     """Return a new matrix with the square of all integers of a matrix."""
     return [list(map(lambda x: x**2, row)) for row in matrix]
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    
