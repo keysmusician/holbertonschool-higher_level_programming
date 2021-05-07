a4e4c8c7caa93a96d4d1e21dc3643c5bcc9c67b8
keysmusician <justin.masayda@gmail.com>
2021-05-07 04:26:12 +0000

    Format

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 def roman_to_int(roman_string):
     """Convert a Roman numeral to an integer."""
     arabic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if type(roman_string) is not str: return 0
+    if type(roman_string) is not str:
+        return 0
 
     ints = [arabic[N] for N in roman_string]
     return sum(map((lambda x, y: -x if x < y else x), ints, ints[1:] + [0]))
