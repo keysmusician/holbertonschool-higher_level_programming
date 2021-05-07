1d10e00e24141a7aa11e35cc4cef849a69cf76df
keysmusician <justin.masayda@gmail.com>
2021-05-07 15:43:37 +0000

    Add task 14

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """Return a new matrix with the square of all integers of a matrix."""
+    return list(map((lambda row: list(map((lambda x: x**2), row))), matrix))
