4e3815671c5a032d8a0291c82d9342fcf0526aad
keysmusician <justin.masayda@gmail.com>
2021-05-08 06:19:14 +0000

    Replace forbidden functions & macros

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,13 +10,15 @@
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t index, len = PyList_Size(p);
+	PyListObject *list;
 
+	list = (PyListObject *)p;
 	printf("[*] Size of the Python List = %li\n", len);
-	printf("[*] Allocated = %li\n", Py_SIZE(p)); /* Incorrect */
+	printf("[*] Allocated = %li\n", list->allocated); /* Incorrect */
 
 	for (index = 0; index < len; index++)
 	{
-		printf("Element %li: %s\n",
-		index, Py_TYPE(PyList_GetItem(p, index))->tp_name);
+		printf("Element %li: %s\n", index,
+			list->ob_item[index]->ob_type->tp_name);
 	}
 }
