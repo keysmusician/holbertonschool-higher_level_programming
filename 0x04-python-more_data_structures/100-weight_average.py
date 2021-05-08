cd65700fbb2dbea95a2fb5cd43be9c055cd58040
keysmusician <justin.masayda@gmail.com>
2021-05-08 06:29:03 +0000

    Try to catch edge case on task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     """Return the weighted average of a list of 2-integer tuples."""
-    return sum([x * y for x, y in my_list]) / sum([y for x, y in my_list])
+    wgt_count = sum([y for x, y in my_list])
+    return sum([x * y for x, y in my_list]) / wgt_count if wgt_count else 0
