1af06f1eb4085865b4280b1ea9ecf62c4997712a
keysmusician <justin.masayda@gmail.com>
2021-05-17 18:22:03 +0000

    Add docstring

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
+    """Print x elements of a list, stopping safely if x > len(my_list)."""
     printed = 0
     for index in range(x):
         try:
