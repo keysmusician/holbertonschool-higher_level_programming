3ef341f05dce3b3bc487e41c65abcda84a70743e
keysmusician <justin.masayda@gmail.com>
2021-05-18 20:53:13 +0000

    Remove redundant checking

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,10 +12,6 @@ class Square:
         Args:
             size (int): A positive integer as the size of the new square.
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
