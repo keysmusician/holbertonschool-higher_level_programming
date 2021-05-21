577dfa6ad97e2caad19322e3d17c1f7433db25cc
keysmusician <justin.masayda@gmail.com>
2021-05-21 13:40:16 +0000

    Add task 5 code, better algorithm

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,22 +10,8 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    # chars = []
-    # for c in text:
-    #     chars.append(c + ("\n\n" if c in ".?:" else ""))
-    # chars = "".join(chars).split("\n\n")
-    # for i, line in enumerate(chars):
-    #     chars[i] = line.lstrip(" ")
-    # formatted_str = "\n\n".join(chars)
-    # print(formatted_str)
-
-
-text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
+    start = 0
+    for i, c in enumerate(text):
+        if c in ".:?":
+            print(text[start:i + 1].lstrip() + "\n")
+            start = i + 1
