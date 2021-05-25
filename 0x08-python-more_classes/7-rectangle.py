83b7f526fae635ed4a6f9c6eb7f2ef71c0e9cbec
keysmusician <justin.masayda@gmail.com>
2021-05-25 02:50:09 +0000

    Remove tests

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -69,28 +69,3 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * self.width + 2 * self.height
-
-
-my_rectangle_1 = Rectangle(8, 4)
-print(my_rectangle_1)
-print("--")
-my_rectangle_1.print_symbol = "&"
-print(my_rectangle_1)
-print("--")
-
-my_rectangle_2 = Rectangle(2, 1)
-print(my_rectangle_2)
-print("--")
-Rectangle.print_symbol = "C"
-print(my_rectangle_2)
-print("--")
-
-my_rectangle_3 = Rectangle(7, 3)
-print(my_rectangle_3)
-
-print("--")
-
-my_rectangle_3.print_symbol = ["C", "is", "fun!"]
-print(my_rectangle_3)
-
-print("--")
