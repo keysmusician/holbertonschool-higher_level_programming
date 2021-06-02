2aa6a2059ab25d3bbaeb989eecbe02cc3395eee6
keysmusician <justin.masayda@gmail.com>
2021-06-02 20:44:22 +0000

    Add task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A class which extends a list."""
+
+
+class BaseGeometry:
+    """Describe basic geometric shapes."""
+
+    def area(self):
+        """Placeholder area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
