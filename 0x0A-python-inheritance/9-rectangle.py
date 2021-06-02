9b71e1b7b4b35d8a0ee89207f7be5bea57331021
keysmusician <justin.masayda@gmail.com>
2021-06-02 21:03:17 +0000

    Add task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""A class which extends a list."""
+
+
+class BaseGeometry:
+    """Describe basic geometric shapes."""
+
+    def area(self):
+        """Placeholder area function."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Ensure values are positive integers."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Describe a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle."""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        """Text description of Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """Calculate area of Rectangle."""
+        return self.__width * self.__height
