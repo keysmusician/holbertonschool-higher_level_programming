a1bdec25a0d8acf24e15570fb2bf4f43874124d0
keysmusician <justin.masayda@gmail.com>
2021-06-02 20:38:59 +0000

    Add task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class which extends a list."""
+
+
+class BaseGeometry:
+    """Describe basic geometric shapes."""
+
+    def area(self):
+        """Placeholder area function"""
+        raise Exception("area() is not implemented")
