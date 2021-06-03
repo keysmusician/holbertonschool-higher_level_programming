49f7cb6cfc97abb2126946e8781c542782ab5019
keysmusician <justin.masayda@gmail.com>
2021-06-03 22:39:14 +0000

    Utilize save_to_json_file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """
-Add all command line arguments to a Python list, and append them to a file.
+Append command line arguments to a JSON list written in a file.
 
 """
-
-
 if __name__ == "__main__":
 
     import json
-    import sys
+    from sys import argv
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__(
         '6-load_from_json_file').load_from_json_file
 
-    with open('add_item.json', 'r') as file:
-        json_list = load_from_json_file('add_item.json')
-    with open('add_item.json', 'w') as file:
-        json.dump(json_list + sys.argv[1:], file)
+    json_list = load_from_json_file('add_item.json')
+    json_list.append(argv[1:])
+    save_to_json_file(json_list, 'add_item.json')
