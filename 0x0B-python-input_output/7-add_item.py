e27accb8a8e5f9d0b6f8e39c181831a4e1fc9558
keysmusician <justin.masayda@gmail.com>
2021-06-03 22:24:39 +0000

    Remove test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -13,6 +13,5 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 with open('add_item.json', 'r') as file:
     json_list = load_from_json_file('add_item.json')
-    print(json_list)
 with open('add_item.json', 'w') as file:
     json.dump(json_list + sys.argv[1:], file)
