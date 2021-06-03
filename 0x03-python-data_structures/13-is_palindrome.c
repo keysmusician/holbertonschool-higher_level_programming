c0b0b899ae223ab5f74c5dfba97db8069ffc4512
keysmusician <justin.masayda@gmail.com>
2021-06-03 03:22:53 +0000

    Add test

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,31 +8,49 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *node, *next, *prev = NULL;
+	listint_t *node, *next, *temp, *prev = NULL;
 	size_t nodecount = 0, midpoint;
 	int odd = 0, result = 1;
 
+	/* If invalid address of head pointer */
 	if (!head)
-		return (1); /* Invalid address of head pointer */
+		return (1);
+
+	/* get length of list */
 	for (node = *head; node; node = node->next)
 		nodecount++;
 	midpoint = nodecount / 2;
 	if (nodecount % 2 != 0)
 		odd = 1;
+
+	/* reverse pointers up to length / 2 */
 	for (node = *head; midpoint--; node = next)
 	{
 		next = node->next;
 		node->next = prev;
 		prev = node;
 	}
+
+	/* compare mid outward, resetting pointers */
 	if (odd)
 		node = node->next;
+
 	while (node)
 	{
+		printf("is_palindrome: node->n = %d\n", node->n);
+		printf("is_palindrome: prev->n = %d\n", prev->n);
 		if (node->n != prev->n && result)
 			result = 0;
+
+		/* Reset pointer direction */
+		next = node;
+		temp = prev->next;
+		prev->next = next;
+		next = prev;
+
 		node = node->next;
-		prev = prev->next;
+		prev = temp->next;
 	}
+
 	return (result);
 }
