eeb8cc68a87460ec961fce338269fcaf9b450015
keysmusician <justin.masayda@gmail.com>
2021-06-03 03:59:10 +0000

    Update 10 init

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -42,6 +42,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialize Square."""
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
