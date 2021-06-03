ef9df9bd502f44a8dbf4ede96d5a613018a2a0dd
keysmusician <justin.masayda@gmail.com>
2021-06-03 23:55:10 +0000

    Use open to check file exists

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,11 @@ Append command line arguments to a JSON list written in a file.
 if __name__ == "__main__":
 
     import json
-    import os.path
     from sys import argv
     save_to_json = __import__('5-save_to_json_file').save_to_json_file
     load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
     jsonlist = []
-    if os.path.exists("add_item.json"):
+    with open("add_item.json"):
         jsonlist = load_from_json("add_item.json")
     save_to_json(jsonlist + argv[1:], 'add_item.json')
