44848f9e793a9abe541308ca732e440b270b51bc
keysmusician <justin.masayda@gmail.com>
2021-06-03 03:57:03 +0000

    Update string format in tas 7 & 8, import 7 in task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,19 +2,7 @@
 """A class which extends a list."""
 
 
-class BaseGeometry:
-    """Describe basic geometric shapes."""
-
-    def area(self):
-        """Placeholder area function."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Ensure values are positive integers."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
