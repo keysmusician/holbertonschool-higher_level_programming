33a9588798e4f187b390197ffca2e79577aeed6b
keysmusician <justin.masayda@gmail.com>
2021-06-03 16:13:56 +0000

    Add task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that reads a text file (UTF8) and prints it to stdout."""
+
+
+def read_file(filename=""):
+    """Read a file."""
+    with open(filename, 'r') as f:
+        print(f.read())
