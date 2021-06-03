a363094ec9e05c4d0d88ff8a2822d0747cee5778
keysmusician <justin.masayda@gmail.com>
2021-06-03 22:27:23 +0000

    Only run when main

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,13 +5,15 @@ Add all command line arguments to a Python list, and append them to a file.
 """
 
 
-import json
-import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
 
+    import json
+    import sys
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__(
+        '6-load_from_json_file').load_from_json_file
 
-with open('add_item.json', 'r') as file:
-    json_list = load_from_json_file('add_item.json')
-with open('add_item.json', 'w') as file:
-    json.dump(json_list + sys.argv[1:], file)
+    with open('add_item.json', 'r') as file:
+        json_list = load_from_json_file('add_item.json')
+    with open('add_item.json', 'w') as file:
+        json.dump(json_list + sys.argv[1:], file)
