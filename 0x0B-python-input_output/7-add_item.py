28bb2808e16387a5a760d3ca1bea469cd82fb952
keysmusician <justin.masayda@gmail.com>
2021-06-03 22:03:04 +0000

    Revert "Do not import func definitions"
    This reverts commit 3863e61d30a7b9c52c77ea899c68950743b3eeb9.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,18 +7,8 @@ Add all command line arguments to a Python list, and append them to a file.
 
 import json
 import sys
-
-
-def save_to_json_file(my_obj, filename):
-    """Write an object to a text file in JSON format."""
-    with open(filename, "w", encoding="utf-8") as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """Create an object from a JSON file."""
-    with open(filename, "r") as file:
-        return json.load(file)
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 with open('add_item.json', 'r') as file:
