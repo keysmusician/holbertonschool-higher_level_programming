7b800331cf73a6aece4b95f559976600f1d0e188
keysmusician <justin.masayda@gmail.com>
2021-06-07 15:09:35 +0000

    update base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
 """
 Base class for project 0x0C. Python - Almost a circle
 
 """
 
+
 class Base:
-    pass
+    """Define a base class to manage the 'id' attribute in derived classes."""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize base class object."""
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
