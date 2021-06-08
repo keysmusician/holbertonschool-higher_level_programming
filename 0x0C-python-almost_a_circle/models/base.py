9653e6cea7c417b8864ce562ed53855df4513021
keysmusician <justin.masayda@gmail.com>
2021-06-08 00:33:17 +0000

    Add task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,9 @@ Base class for project 0x0C. Python - Almost a circle.
 """
 
 
+import json
+
+
 class Base:
     """Define a base class to manage the 'id' attribute in derived classes."""
 
@@ -17,3 +20,18 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON representation of a list of dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save 'list_objs' to a file in JSON format."""
+        with open("{}.json".format(cls.__name__), 'w+') as file:
+            dict_list = [obj.to_dictionary() for obj in list_objs]
+            json_str = cls.to_json_string(dict_list)
+            file.write(json_str)
