8c9b8f7db0b9bb2384937784d21233e6d6c75497
keysmusician <justin.masayda@gmail.com>
2021-06-09 18:44:07 +0000

    Use __class__ instead of type(self)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,8 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            type(self).__nb_objects += 1
-            self.id = type(self).__nb_objects
+            __class__.__nb_objects += 1
+            self.id = __class__.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
