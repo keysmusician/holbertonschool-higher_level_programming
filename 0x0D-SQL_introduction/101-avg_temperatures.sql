95c2549519869dbd9f300644f113fbe907bcd11c
keysmusician <justin.masayda@gmail.com>
2021-07-14 04:37:38 +0000

    Add task 18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Display the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT DISTINCT city, AVG(value) as avg_temp FROM temperatures WHERE month BETWEEN 7 and 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
