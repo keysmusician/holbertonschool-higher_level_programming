d65530968ee5294ebe73eaed1efb2d7a69b62f8f
keysmusician <justin.masayda@gmail.com>
2021-07-15 02:31:27 +0000

    Select id, name

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
---  List all the cities of California in the selected database
-SELECT * FROM cities WHERE state_id = (
+--  List all the cities of California.
+SELECT id, name FROM cities WHERE state_id = (
 	SELECT id FROM states WHERE name = "California"
 ) ORDER BY cities.id;
