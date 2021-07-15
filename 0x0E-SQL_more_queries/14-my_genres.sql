471f7e4d43e741d295a05e9bc412c0367dc5a000
keysmusician <justin.masayda@gmail.com>
2021-07-15 02:04:18 +0000

    Add task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- List all genres of the show Dexter.
+SELECT tv_genres.name FROM tv_show_genres
+	JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	WHERE show_id = (
+	SELECT id FROM tv_shows WHERE title = "Dexter"
+	ORDER BY name
+);
