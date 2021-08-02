03ee7ddb4a29a657cc6a81ea8a304599b57318ec
keysmusician <justin.masayda@gmail.com>
2021-08-02 22:19:37 +0000

    Add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+
+if (!size) {
+  console.log('Missing size');
+}
+
+for (let row = size; row > 0; row--) {
+  console.log('x'.repeat(size));
+}
