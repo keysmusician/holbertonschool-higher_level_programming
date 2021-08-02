36804b86f27fb5d4fdbe157c2d0a8a1b71b8860d
keysmusician <justin.masayda@gmail.com>
2021-08-02 21:17:21 +0000

    Add task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let argc = process.argv.length
+if (argc <= 2) {
+	console.log('No argument')
+} else if (argc === 3) {
+	console.log('Argument found')
+} else {
+	console.log('Arguments found')
+}
