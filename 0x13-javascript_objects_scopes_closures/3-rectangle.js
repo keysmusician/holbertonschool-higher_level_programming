a60b2562dd9c4f25a1132db3ac39d8f4079fadc1
keysmusician <justin.masayda@gmail.com>
2021-08-03 19:50:21 +0000

    Add task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let rows = this.height; rows; rows--) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
