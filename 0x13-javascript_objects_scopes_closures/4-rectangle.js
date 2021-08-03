bbbc0bf366531233b201563178aaaf335cec5bdc
keysmusician <justin.masayda@gmail.com>
2021-08-03 19:58:38 +0000

    Remove tests

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -26,15 +26,3 @@ class Rectangle {
 }
 
 module.exports = Rectangle;
-
-const r1 = new Rectangle(2, 3);
-console.log('Normal:');
-r1.print();
-
-console.log('Double:');
-r1.double();
-r1.print();
-
-console.log('Rotate:');
-r1.rotate();
-r1.print();
