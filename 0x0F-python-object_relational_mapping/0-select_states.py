644de6e54170799ca8b997837759c9076090ace7
keysmusician <justin.masayda@gmail.com>
2021-08-05 22:21:17 +0000

    Add task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Lists all states from the database `hbtn_0e_0_usa`."""
+import MySQLdb
+import sys
+
+
+username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+db = MySQLdb.connect(user=username, passwd=password, db=database)
+db.query("""SELECT * FROM states ORDER BY states.id ASC;""")
+rows = db.store_result()
+for row in rows.fetch_row(0):
+    print(row)
