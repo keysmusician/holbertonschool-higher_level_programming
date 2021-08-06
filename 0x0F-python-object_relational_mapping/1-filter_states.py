927aa6d9f8516c9569a8a9e1aa6f74a6c3bae7cf
keysmusician <justin.masayda@gmail.com>
2021-08-06 01:00:58 +0000

    Add task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name starting with 'N' from the database
+`hbtn_0e_0_usa`.
+
+"""
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
+    db.query("""SELECT * FROM states ORDER BY states.id ASC;""")
+    rows = db.store_result()
+    for row in rows.fetch_row(0):
+        if row[1][0] == "N":
+            print(row)
