bf35e472c23fafb97b01a9b32dae6662044a67d4
keysmusician <justin.masayda@gmail.com>
2021-08-06 01:49:10 +0000

    Add comments

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,15 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
 
+    # Connect to MySQL database based on command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(user=username, passwd=password, db=database)
+
+    # Run the following query:
     query = """
             SELECT *
             FROM states
@@ -22,5 +25,7 @@ if __name__ == "__main__":
             """.format(state_name)
     db.query(query)
     result = db.store_result()
+
+    # Print the results
     for row in result.fetch_row(0):
         print(row)
