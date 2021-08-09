7e242ae2d3a887f6f22c759cb91259f349fdf573
keysmusician <justin.masayda@gmail.com>
2021-08-09 15:06:48 +0000

    Update documentation

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Change the name of a State object from the database `hbtn_0e_6_usa`."""
+"""Change the name of a `State` object from the database `hbtn_0e_6_usa`."""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
