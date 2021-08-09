24d86997ece9c85f37d9e5d2b26b5d860b9c76e6
keysmusician <justin.masayda@gmail.com>
2021-08-09 15:01:28 +0000

    Update documentation

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""List all State objects from the database `hbtn_0e_6_usa`."""
+"""Print the first `State` object from the database `hbtn_0e_6_usa`."""
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
