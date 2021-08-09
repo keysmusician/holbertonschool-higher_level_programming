7631f2efcd912eb3d2c77124095fe5285abf191d
keysmusician <justin.masayda@gmail.com>
2021-08-09 04:02:15 +0000

    Add model_city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class definition of a `City`."""
+import sqlalchemy
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """`City` class corresponding to MySQL `cities` table."""
+    __tablename__ = "cities"
+
+    id = sqlalchemy.Column(
+        Integer, unique=True, primary_key=True, autoincrement=True
+    )
+    name = sqlalchemy.Column(String(128), nullable=False)
+    state_id = sqlalchemy.Column(
+        Integer, ForeignKey("states.id"), nullable=False
+    )
