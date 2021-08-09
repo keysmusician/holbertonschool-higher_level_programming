ee73a8b35daaf197d883862a90465c2e1bf8fa18
keysmusician <justin.masayda@gmail.com>
2021-08-09 01:58:35 +0000

    Add task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Class definition of a `State`. Connect to a DB using SQL Alchemy."""
+import sqlalchemy
+from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+
+    id = sqlalchemy.Column(
+        Integer, unique=True, primary_key=True, autoincrement=True
+    )
+    name = sqlalchemy.Column(String(128), nullable=False)
