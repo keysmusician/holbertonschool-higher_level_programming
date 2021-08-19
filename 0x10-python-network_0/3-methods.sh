b33ed9839c76e47a6dc1a49ea76addd3ac277e0c
keysmusician <justin.masayda@gmail.com>
2021-08-19 22:13:07 +0000

    Accept a parameter

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Take a URL and display all HTTP methods the server will accept
-curl -sIX OPTIONS 0:5000 | grep Allow: | cut -d " " -f 2-
+curl -sIX OPTIONS "$1" | grep Allow: | cut -d " " -f 2-
