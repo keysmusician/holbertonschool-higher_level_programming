ebf7af8bd7407001d380895f56faeedf85ec9428
keysmusician <justin.masayda@gmail.com>
2021-08-20 19:57:07 +0000

    Do not run on import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,8 @@ variable found in the header of the response.
 import sys
 import urllib.request
 
-url = sys.argv[1]
 
-with urllib.request.urlopen(url) as request:
-    print(request.getheader('X-Request-Id'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
