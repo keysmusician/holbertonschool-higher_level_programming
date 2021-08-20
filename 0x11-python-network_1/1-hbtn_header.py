9569729458783fb2305739ad4039e80be0735d99
keysmusician <justin.masayda@gmail.com>
2021-08-20 22:11:04 +0000

    Update docstring

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Sends a request to a given URL and displays the value of the X-Request-Id
+Sends a request to a given URL and displays the value of the "X-Request-Id"
 variable found in the header of the response.
 
 """
