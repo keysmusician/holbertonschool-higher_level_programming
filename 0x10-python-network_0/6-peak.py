152c30f8b41181e4123457a5507d81e2309705ea
keysmusician <justin.masayda@gmail.com>
2021-08-20 03:44:10 +0000

    Use sort based implementation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,26 +5,6 @@
 def find_peak(list_of_integers):
     """Find a peak in a list of integers."""
     try:
-        peak = None
-        prev_slope = 0
-        for i, n in enumerate(list_of_integers):
-            # print("index: {}; n: {}".format(i, n))
-
-            # Skip first number, but save it as peak in case it's the only one
-            if i == 0:
-                peak = n
-                continue
-
-            # Calculate slope
-            prev_n = list_of_integers[i - 1]
-            slope = n - prev_n
-
-            # If slope is positive or 0
-            if slope >= 0:
-                peak = n
-            elif prev_slope > 0:
-                return prev_n
-            prev_slope = slope
-        return peak
-    except(TypeError):
+        return sorted(list_of_integers)[-1]
+    except:
         return None
