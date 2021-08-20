33dc410b985bc8f41b66c271071fddb744dd5472
keysmusician <justin.masayda@gmail.com>
2021-08-20 03:50:31 +0000

    Use builtin sort

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,7 @@
 def find_peak(list_of_integers):
     """Find a peak in a list of integers."""
     try:
-        return sorted(list_of_integers)[-1]
+        list_of_integers.sort()
+        return list_of_integers[-1]
     except:
         return None
