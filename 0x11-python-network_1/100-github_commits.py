bfa8d4fe0f3f57562955c064a7a8a05fa1726ef7
keysmusician <justin.masayda@gmail.com>
2021-08-21 00:42:52 +0000

    Add task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Prints 10 commits."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    repo = argv[2]
+    url = "http://api.github.com/repos/{}/{}/commits".format(user, repo)
+    response = requests.get(url)
+    commits = response.json()
+    for idx, commit in enumerate(commits):
+        if idx == 10:
+            break
+        print("{}: {}".format(
+            commit.get("sha"), commit.get("committer").get("login"))
+        )
