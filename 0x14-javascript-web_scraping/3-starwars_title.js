3cf45e7f81e69a0c8a5b33491a26871bcb9cd561
keysmusician <justin.masayda@gmail.com>
2021-09-14 16:54:45 +0000

    Semistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 // Print the title of a Star Wars movie where the episode number matches a
 // given integer.
-const request = require('request')
-n = process.argv[2]
-url = 'https://swapi-api.hbtn.io/api/films/'.concat(n)
+const request = require('request');
+const n = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/'.concat(n);
 request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
   console.log(JSON.parse(body).title);
 });
