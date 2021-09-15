11cf592389e9ec0952f48768ac633957ae8c5cfe
keysmusician <justin.masayda@gmail.com>
2021-09-15 01:29:08 +0000

    Revert "Filter in script rather than with request"
    This reverts commit 81f4d2d83ef34e646c2f1d7818e6d45ab16c4ae8.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,14 @@
 #!/usr/bin/node
 // Print the number of completed tasks per user.
 const request = require('request');
-const apiUrl = process.argv[2];
+const apiUrl = process.argv[2].concat('?completed=true');
 request(apiUrl, (error, response, body) => {
   if (error) return console.error(error);
   const tasks = JSON.parse(body);
   const result = {};
   for (const task of tasks) {
-    if (task.completed) {
-      const key = task.userId;
-      result[key] ? result[key]++ : result[key] = 1;
-    }
+    const key = String(task.userId);
+    result[key] ? result[key]++ : result[key] = 1;
   }
   console.log(result);
 });
