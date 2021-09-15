ee3af5fc72967df6488a86f21973bbf796c676f8
keysmusician <justin.masayda@gmail.com>
2021-09-15 01:18:01 +0000

    Revert "Test solution"
    This reverts commit 6d0bdc39d5386fc3b6307dd16761013fcf33b02b.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,14 @@
 #!/usr/bin/node
+// Print the number of completed tasks per user.
 const request = require('request');
-const GET = request.get;
-const url = process.argv[2];
-
-GET(url, (err, res, body) => {
-  if (err) return (console.log(err));
-  const data = JSON.parse(body);
-  const tasks = {};
-
-  for (let x = 0; x < data.length; x++) {
-    if (data[x].completed) {
-      const id = data[x].userId;
-      tasks[id] !== undefined ? tasks[id] += 1 : tasks[id] = 1;
-    }
+const apiUrl = process.argv[2].concat('?completed=true');
+request(apiUrl, (error, response, body) => {
+  if (error) return console.error(error);
+  const tasks = JSON.parse(body);
+  const result = {};
+  for (const task of tasks) {
+    const key = String(task.userId);
+    result[key] ? result[key]++ : result[key] = 1;
   }
-  console.log(tasks);
+  console.log(result);
 });
